cf8cbb7c04209addb501e9207fc7fc93810dbace
romel-romodulo <57033860+romodulo@users.noreply.github.com>
2025-03-09 13:32:22 +1000

    First commit of < 3-9>  <13:31>  :  Modify python-time--tracker(py-file); create businessThoughts Dir + businessThoughts01: File

--- python_time_tracker/python_time_tracker.py
+++ python_time_tracker/python_time_tracker.py
@@ -12,7 +12,7 @@ from decimal import Decimal, ROUND_DOWN
 # postpone task: # create match case
 
 def run_CheckA(previous_timeStamp_Data, today_timeStamp_Data, final_outcome=False):
-    def arbFunc(previous_timeStamp_Data, today_timeStamp_Data, final_outcome=False):
+    def arbFunc_False(previous_timeStamp_Data, today_timeStamp_Data):
         #check two-time-stamps:
         print(p := previous_timeStamp_Data)
         print(n := today_timeStamp_Data)
@@ -36,14 +36,40 @@ def run_CheckA(previous_timeStamp_Data, today_timeStamp_Data, final_outcome=Fals
         seconds_calc = seconds_calc.quantize(precision, rounding=ROUND_DOWN)
         print(seconds_calc)
         print(str(int((c/60)//60)) + "h", str(((c/60)%60)//1) + "m", str(seconds_calc) + "s")
+    def arbFunc_True(previous_timeStamp_Data, today_timeStamp_Data):
+        #check two-time-stamps:
+        # print(p := previous_timeStamp_Data) #un-commented: 3-9 12:34
+        p = previous_timeStamp_Data
+        # print(n := today_timeStamp_Data) #un-commented: 3-9 12:34
+        n = today_timeStamp_Data
+        # print(today.date())
+        # print(today.time())
+        # print(datetime.fromtimestamp(p))
+        # print(datetime.fromtimestamp(n))
+        pre_c = n-p
+        c = Decimal.from_float(pre_c)
+        precision = Decimal('0.01')
+        c = c.quantize(precision, rounding=ROUND_DOWN)
+        # print(c) #un-commented: 3-9 12:34
+        denominator = Decimal.from_float(60.000)
+        denominator = denominator.quantize(precision, rounding=ROUND_DOWN)
+        # print(denominator) #un-commented: 3-9 12:34
+        # print(c/denominator) #un-commented: 3-9 12:34
+        # print(str(int((c/60)//60)) + "h", str(((c/60)%60)//1) + "m", str((((c/60)%60)%1)*60) + "s")
+        # print(str(int((c/denominator)//denominator)) + "h", (((c/denominator)%denominator)),"m")
+        precision = Decimal('0')
+        seconds_calc = ((((c/60)%60)%1)*60)
+        seconds_calc = seconds_calc.quantize(precision, rounding=ROUND_DOWN)
+        # print(seconds_calc) #un-commented: 3-9 12:34
+        print(str(int((c/60)//60)) + "h", str(((c/60)%60)//1) + "m", str(seconds_calc) + "s")
     if not final_outcome:
-        arbFunc(previous_timeStamp_Data, today_timeStamp_Data, final_outcome=False)
+        arbFunc_False(previous_timeStamp_Data, today_timeStamp_Data)
     else:
-        arbFunc(previous_timeStamp_Data, today_timeStamp_Data, final_outcome=False)
+        arbFunc_True(previous_timeStamp_Data, today_timeStamp_Data)
 
 
 
-def definitionA(fileName_arg, run_CheckA__Switch = False):
+def definitionA(fileName_arg, run_CheckA__Switch = False, final_outcome = False):
     #initial-variables:
     previous_timeStamp_Data = None
     #
@@ -63,7 +89,7 @@ def definitionA(fileName_arg, run_CheckA__Switch = False):
         if previous_timeStamp_Data:
             run_CheckA(previous_timeStamp_Data, today_timeStamp_Data)
     print("")
-def definitionB(fileName_arg, run_CheckA__Switch = False, close__Switch = False):
+def definitionB(fileName_arg, run_CheckA__Switch = False, final_outcome=False, close__Switch = False):
     #initial-variables:
     previous_timeStamp_Data = None
     #
@@ -92,10 +118,10 @@ def definitionB(fileName_arg, run_CheckA__Switch = False, close__Switch = False)
             previous_timeStamp_Data = timeStamp_Data['timeStamp_Data']
     if run_CheckA__Switch:
         if previous_timeStamp_Data:
-            run_CheckA(previous_timeStamp_Data, today_timeStamp_Data)
+            run_CheckA(previous_timeStamp_Data, today_timeStamp_Data, final_outcome)
     if close__Switch:
         with shelve.open(fileName) as openedFile:
-            openedFile['time--delta'] = previous_timeStamp_Data - today_timeStamp_Data
+            openedFile['time--delta'] = today_timeStamp_Data - previous_timeStamp_Data
             openedFile['close--time'] = today_timeStamp_Data
     print("")
 
@@ -146,7 +172,9 @@ if __name__ == '__main__':
     # definitionB("2nd_break", run_CheckA__Switch = True, close__Switch = True)
     # do not uncomment:
     # definitionB("walking_timer_1", run_CheckA__Switch = True, close__Switch = True)
-    definitionB("arb_timer", run_CheckA__Switch=True)
+    # do not uncomment:
+    # definitionB("arb_timer", run_CheckA__Switch=True, close__Switch=True)
+    #
     time__delta, closing__time = open_Data("2nd_break", printName = True)
     print(time__delta)
     print(n := closing__time, "\n")
@@ -173,6 +201,32 @@ if __name__ == '__main__':
     print(n-t)
     print("")
     run_CheckA(t, n)
+
+    print("")
+    # save-name: laziTimer_beforeSleep_3_8  # timer didn't save
+    #
+    # do not uncomment
+    # definitionB("runSleepTimer", run_CheckA__Switch=True, close__Switch=True)
+
+    #
+    #
+    time__delta, closing__time = open_Data("runSleepTimer", printName = True)
+    print(time__delta)
+    print(n := closing__time, "\n")
+    timeStamp_Data = open_Data_timeStamp("runSleepTimer", printName=True)
+    print(t := timeStamp_Data)
+    print(n-t)
+    print("")
+    run_CheckA(t, n)
+
+    #
+    #
+    print("")
+    definitionB("dailyClock_3_9", run_CheckA__Switch=True, close__Switch=False)
+    definitionB("./notWorking/notWorkingTimer01", run_CheckA__Switch=True, final_outcome=True, close__Switch=False)
+
+
+
 
     #
     # I was able to successfully migrate: time_StampData, 2nd_break, arb_timer
